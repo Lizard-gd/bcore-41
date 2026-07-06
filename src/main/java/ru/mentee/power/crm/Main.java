5fd0416dba19456ff2d04f9ef7e93db2044b0aed
Gleb Druzhinin <lizardgd99@gmail.com>
2026-07-06 19:33:49 +0300

    BCORE-41-code-review-4: Код-ревью REST контроллера
    - Добавил @EnableJpaAuditing – включил механизм аудита.
    - Заменил @PrePersist/@PreUpdate на @CreatedDate/@LastModifiedDate.
    - Добавил @EntityListeners(AuditingEntityListener.class) на сущность.
    - Заменил Instant на LocalDateTime.
    - Обновил InviteeResponse.
    - Удалил методы onCreate() и onUpdate().

--- src/main/java/ru/mentee/power/crm/Main.java
+++ src/main/java/ru/mentee/power/crm/Main.java
@@ -1,17 +1,13 @@
 package ru.mentee.power.crm;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+@SpringBootApplication
+@EnableJpaAuditing
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
